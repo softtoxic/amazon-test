da2233702f04b6e12c63ff27c4df13cdba872f1c
softtoxic <lakhanijaydip09@gmail.com>
2023-07-26 22:28:43 +0100

    i check it

--- src/Nevil_Challenge.java
+++ src/Nevil_Challenge.java
@@ -2,5 +2,6 @@ public class Nevil_Challenge {
 
     public static void main(String[] args) {
         System.out.println("Hello Guys");
+        System.out.println("I did all changes those you want, thank you");
     }
 }
